2bbd5115fb600cd65884169a7c2337347dfb2f0a
koya1616 <k_haru_love@i.softbank.jp>
2022-04-03 20:54:16 +0900

    f

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "/github.com/gin-gonic/gin"
+
+import "net/http"
+
+func main() {
+    engine:= gin.Default()
+    // htmlのディレクトリを指定
+    engine.LoadHTMLGlob("templates/*")
+    engine.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.html", gin.H{
+             // htmlに渡す変数を定義
+            "message": "hello gin",
+        })
+    })
+    engine.Run(":3010")
+}
